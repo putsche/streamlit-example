a1e3168ee834acbec3d1e60e24079f0d95edf6e0
putsche <49721541+putsche@users.noreply.github.com>
2025-05-16 18:51:34 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -7,13 +7,13 @@ import streamlit as st
 def login_screen():
     st.subheader("This app is private.")
     st.write("Please log in to proceed ...")
-    st.button("Sign in with your Microsoft", on_click=st.login)
+    st.button("Sign in with Microsoft", on_click=st.login)
 
 if not st.user.is_logged_in:
     login_screen()
 else:
-    st.write(f"Welcome, {st.user.name}!")
-    st.caption("Details")
+    st.write(f"Welcome to Parexel: {st.user.name}!")
+    st.caption("User details")
     st.user
     if st.button("Sign out"):
         st.logout()
