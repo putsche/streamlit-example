26f3dc959343d1f805984721309d633b1cbd0e82
putsche <49721541+putsche@users.noreply.github.com>
2025-05-16 17:21:57 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,18 @@ import math
 import pandas as pd
 import streamlit as st
 
+def login_screen():
+    st.header("This app is private.")
+    st.subheader("Please log in.")
+    st.button("Log in with Microsoft", on_click=st.login)
+
+if not st.user.is_logged_in:
+    login_screen()
+else:
+    st.user
+    st.header(f"Welcome, {st.user.name}!")
+
+
 """
 # Welcome to Streamlit
 
